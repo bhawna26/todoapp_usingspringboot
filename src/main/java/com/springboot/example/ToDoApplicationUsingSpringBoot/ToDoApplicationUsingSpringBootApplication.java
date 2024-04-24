51ac275f7e988c2a57d0d989bbc99bcdc0eaca1d
Bhawna <71230093+bhawna26@users.noreply.github.com>
2024-04-24 09:45:17 +0530

    first commit

---
+++ src/main/java/com/springboot/example/ToDoApplicationUsingSpringBoot/ToDoApplicationUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.example.ToDoApplicationUsingSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApplicationUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApplicationUsingSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/example/ToDoApplicationUsingSpringBoot/ToDoApplicationUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.example.ToDoApplicationUsingSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationUsingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
